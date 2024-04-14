872eb4eb0f7a8dedeeb8879f36a4d1c1eabe03f0
Solomon Awah <85108801+solomedia-account@users.noreply.github.com>
2024-04-14 23:41:22 +0100

    Add files via upload
    Tried to make the first commits with IDEA and was very slow or needed to configure it properly.

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.)
+})
+
+app.listen(5005, ()=> console.log("The express and Node server is running on port 5005!"));
